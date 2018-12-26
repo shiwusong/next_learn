e19c4946fa9cf48b5c42754bbfa710ab67a811a1
shiwusong <shiwusong@bytedance.com>
2018-12-26 21:47:15 +0800

    save

--- core/story.js
+++ core/story.js
@@ -2,11 +2,26 @@ import React from 'react'
 export const context = React.createContext()
 export const Provider = context.Provider
 export const connect = Com => {
-  return () => (
-    <context.Consumer>
-      {value => {
-        return <Com value={value} />
-      }}
-    </context.Consumer>
-  )
+	return props => (
+		<context.Consumer>
+			{value => {
+				return <Com value={value} {...props} />
+			}}
+		</context.Consumer>
+	)
+}
+
+const GlobalContext = {}
+export const createStory = modules => {
+	if (!modules) throw new Error('No story params!')
+
+	Object.entries(modules).forEach(([name, module]) => {
+		// 1. mounting state
+		GlobalContext[name] = GlobalContext[name] || {}
+		if (!module.state || typeof module.state != 'function') throw new Error(`[${name}] has no state function`)
+		GlobalContext[name].state = module.state()
+		// 2. mounting mutations
+		if (module.mutations) {
+		}
+	})
 }
--- index.js
+++ index.js
@@ -1,55 +1,63 @@
-const fastify = require('fastify')({ logger: { level: 'error' } })
-const Next = require('next')
-const serveStatic = require('serve-static')
-const path = require('path')
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-fastify.use('/public', serveStatic(path.resolve(__dirname, 'public')))
-fastify.register((fastify, opts, next) => {
-  const app = Next({ dev })
-  app
-    .prepare()
-    .then(_ => {
-      if (dev) {
-        fastify.get('/_next/*', (req, reply) => {
-          return app.handleRequest(req.req, reply.res).then(_ => {
-            reply.sent = true
-          })
-        })
-      }
+module.exports = async (app, options) => {
+	app.register(require('./server/index'), {
+		prefix: '/api',
+	}).after(err => {
+		console.error('[modules] ./server/index register failed!')
+	})
+}
 
-      fastify.get('/a', (req, reply) => {
-        return app.render(req.req, reply.res, '/about', req.query).then(() => {
-          reply.sent = true
-        })
-      })
+// const fastify = require('fastify')({ logger: { level: 'error' } })
+// const Next = require('next')
+// const serveStatic = require('serve-static')
+// const path = require('path')
+// const port = parseInt(process.env.PORT, 10) || 3000
+// const dev = process.env.NODE_ENV !== 'production'
+// fastify.use('/public', serveStatic(path.resolve(__dirname, 'public')))
+// fastify.register((fastify, opts, next) => {
+//   const app = Next({ dev })
+//   app
+//     .prepare()
+//     .then(_ => {
+//       if (dev) {
+//         fastify.get('/_next/*', (req, reply) => {
+//           return app.handleRequest(req.req, reply.res).then(_ => {
+//             reply.sent = true
+//           })
+//         })
+//       }
 
-      fastify.get('/*', (req, reply) => {
-        return app.handleRequest(req.req, reply.res).then(() => {
-          reply.sent = true
-        })
-      })
+//       fastify.get('/a', (req, reply) => {
+//         return app.render(req.req, reply.res, '/about', req.query).then(() => {
+//           reply.sent = true
+//         })
+//       })
 
-      fastify.setNotFoundHandler((request, reply) => {
-        return app.render404(request.req, reply.res).then(() => {
-          reply.sent = true
-        })
-      })
+//       fastify.get('/*', (req, reply) => {
+//         return app.handleRequest(req.req, reply.res).then(() => {
+//           reply.sent = true
+//         })
+//       })
 
-      next()
-    })
-    .catch(err => next(err))
-})
+//       fastify.setNotFoundHandler((request, reply) => {
+//         return app.render404(request.req, reply.res).then(() => {
+//           reply.sent = true
+//         })
+//       })
 
-fastify.register(require('./server'), {
-  prefix: '/api'
-})
+//       next()
+//     })
+//     .catch(err => next(err))
+// })
 
-fastify.addHook('onRequest', async (req, res) => {
-  console.log('url:', req.url)
-})
+// fastify.register(require('./server'), {
+//   prefix: '/api'
+// })
 
-fastify.listen(port, err => {
-  if (err) throw err
-  console.log(`> Ready on http://localhost:${port}`)
-})
+// fastify.addHook('onRequest', async (req, res) => {
+//   console.log('url:', req.url)
+// })
+
+// fastify.listen(port, err => {
+//   if (err) throw err
+//   console.log(`> Ready on http://localhost:${port}`)
+// })
